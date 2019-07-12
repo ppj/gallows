5e1f5ffb827a53f777d4cb01832e6134e4d7cc51
Prasanna Joshi <prasanna_3003@yahoo.com>
2019-07-12 18:59:07 +1000

    Option to start a new game if current is over

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -9,6 +9,14 @@ defmodule GallowsWeb.HangmanView do
     already_used: {:info, "Already guessed."},
   }
 
+  def game_over?(state) do
+    state in [:won, :lost]
+  end
+
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game))
+  end
+
   def display_word(letters) do
     letters |> Enum.join(" ")
   end
