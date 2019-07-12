926c5142bb1a0317744ce405d58315ed16e0c1e4
Prasanna Joshi <prasanna_3003@yahoo.com>
2019-07-12 18:13:37 +1000

    Helper for alerts/banners

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -1,7 +1,33 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
+  @responses %{
+    won: {:success, "You won!"},
+    lost: {:danger, "You lost!"},
+    good_guess: {:success, "Good guess!"},
+    bad_guess: {:warning, "Bad guess!"},
+    already_used: {:info, "Already guessed."},
+  }
+
   def display_word(letters) do
     letters |> Enum.join(" ")
+  end
+
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  defp alert({class, message}) do
+    """
+    <div class="alert, alert-#{class}">
+      #{message}
+    </div>
+    """
+    |> raw()
+  end
+
+  defp alert(nil) do
+    ""
   end
 end
